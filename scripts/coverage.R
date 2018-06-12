19f8326d2f079389df913634f0f74c50a1ae5453
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-06-12 07:22:42 +0100

    Use up-to-date covr function

--- scripts/coverage.R
+++ scripts/coverage.R
@@ -2,4 +2,4 @@
 cov <- covr::package_coverage(
   line_exclusions = dir("src/lmdb", full.names = TRUE, pattern = "\\.c$"))
 print(cov)
-covr::shine(cov, "coverage.html", browse = TRUE)
+covr::report(cov, "coverage.html", browse = TRUE)
