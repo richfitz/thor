9391cc3128371998ca2d219306b13c00f3626604
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-09-06 13:02:06 +0100

    Enable strict headers

--- src/util.c
+++ src/util.c
@@ -17,14 +17,14 @@ bool no_error2(int rc, int false_flag, const char* str) {
 }
 
 const char * scalar_character(SEXP x, const char * name) {
-  if (TYPEOF(x) != STRSXP || length(x) != 1) {
+  if (TYPEOF(x) != STRSXP || Rf_length(x) != 1) {
     Rf_error("Expected a scalar character for '%s'", name);
   }
   return CHAR(STRING_ELT(x, 0));
 }
 
 int scalar_int(SEXP x, const char * name) {
-  if (TYPEOF(x) != INTSXP || length(x) != 1) {
+  if (TYPEOF(x) != INTSXP || Rf_length(x) != 1) {
     Rf_error("Expected a scalar integer for '%s'", name);
   }
   return INTEGER(x)[0];
@@ -33,13 +33,13 @@ int scalar_int(SEXP x, const char * name) {
 
 mdb_size_t scalar_mdb_size(SEXP x, const char * name) {
   mdb_size_t ret = 0;
-  if (TYPEOF(x) == INTSXP && length(x) == 1) {
+  if (TYPEOF(x) == INTSXP && Rf_length(x) == 1) {
     int value = INTEGER(x)[0];
     if (value < 0) {
       Rf_error("Expected a positive size for '%s'", name);
     }
     ret = value;
-  } else if (TYPEOF(x) == REALSXP && length(x) == 1) {
+  } else if (TYPEOF(x) == REALSXP && Rf_length(x) == 1) {
     double value = REAL(x)[0];
     if (value < 0) {
       Rf_error("Expected a positive size for '%s'", name);
@@ -62,7 +62,7 @@ size_t scalar_size(SEXP x, const char * name) {
 }
 
 bool scalar_logical(SEXP x, const char * name) {
-  if (TYPEOF(x) != LGLSXP || length(x) != 1) {
+  if (TYPEOF(x) != LGLSXP || Rf_length(x) != 1) {
     Rf_error("Expected a scalar logical for '%s'", name);
   }
   int ret = INTEGER(x)[0];
@@ -76,7 +76,7 @@ SEXP r_is_null_pointer(SEXP x) {
   if (TYPEOF(x) != EXTPTRSXP) {
     Rf_error("Expected an external pointer");
   }
-  return ScalarLogical(R_ExternalPtrAddr(x) == NULL);
+  return Rf_ScalarLogical(R_ExternalPtrAddr(x) == NULL);
 }
 
 return_as to_return_as(SEXP x) {
@@ -110,13 +110,13 @@ SEXP raw_string_to_sexp(const char *str, size_t len, return_as as_raw) {
   bool is_raw = is_raw_string(str, len, as_raw);
   SEXP ret;
   if (is_raw) {
-    ret = PROTECT(allocVector(RAWSXP, len));
+    ret = PROTECT(Rf_allocVector(RAWSXP, len));
     memcpy(RAW(ret), str, len);
   } else if (as_raw == AS_CHAR) {
-    ret = PROTECT(mkCharLen(str, len));
+    ret = PROTECT(Rf_mkCharLen(str, len));
   } else {
-    ret = PROTECT(allocVector(STRSXP, 1));
-    SET_STRING_ELT(ret, 0, mkCharLen(str, len));
+    ret = PROTECT(Rf_allocVector(STRSXP, 1));
+    SET_STRING_ELT(ret, 0, Rf_mkCharLen(str, len));
   }
   UNPROTECT(1);
   return ret;
@@ -130,16 +130,16 @@ size_t sexp_get_data(SEXP data, const char **data_contents, const char* name) {
     // return length(data);
     // break;
   case STRSXP:
-    if (length(data) != 1) {
+    if (Rf_length(data) != 1) {
       Rf_error("'%s' must be a scalar character", name);
     }
     SEXP el = STRING_ELT(data, 0);
     *data_contents = CHAR(el);
-    return length(el);
+    return Rf_length(el);
     break;
   case RAWSXP:
     *data_contents = (const char*) RAW(data);
-    return length(data);
+    return Rf_length(data);
   default:
     Rf_error("Invalid data type for '%s'; expected string or raw", name);
   }
@@ -155,20 +155,20 @@ SEXP r_test_error(SEXP r_rc, SEXP r_false_flag, SEXP r_str) {
     int false_flag = scalar_int(r_false_flag, "false_flag");
     ret = no_error2(rc, false_flag, str);
   }
-  return ScalarLogical(ret);
+  return Rf_ScalarLogical(ret);
 }
 
 SEXP combine_vector(SEXP x, size_t len) {
-  if (len == (size_t)length(x)) {
+  if (len == (size_t)Rf_length(x)) {
     return x;
   }
   bool out_str = TYPEOF(x) == STRSXP;
-  SEXP ret = PROTECT(allocVector(TYPEOF(x), len));
-  size_t n = length(x);
+  SEXP ret = PROTECT(Rf_allocVector(TYPEOF(x), len));
+  size_t n = Rf_length(x);
   for (size_t i = 0, j = 0; i < len; ++i, ++j) {
     if (j == n) {
-      x = getAttrib(x, install("next"));
-      n = length(x);
+      x = Rf_getAttrib(x, Rf_install("next"));
+      n = Rf_length(x);
       j = 0;
     }
     if (out_str) {
@@ -188,5 +188,5 @@ SEXP r_pointer_addr_str(SEXP r_ptr) {
   void * ptr = R_ExternalPtrAddr(r_ptr);
   char buf[128];
   snprintf(buf, 128, "%p", ptr);
-  return mkString(buf);
+  return Rf_mkString(buf);
 }
